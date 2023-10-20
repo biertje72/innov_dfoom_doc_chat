d18a3818d2efd2b2ab0feb1d424488fa03979812
Maurice <biertje72@gmail.com>
2023-10-20 14:39:20 +0000

    Adding restart script

---
+++ restart_localgtp_api.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Process found:"
+pgrep -u ec2-user -af "python3 run_localGPT_API.py" 
+
+echo "Do you want to restart it? (y/n)"
+read -r answer
+
+if [[ "$answer" == "y" ]]; then
+  # Do something
+  echo "Killing the process"
+  pkill -u ec2-user -f "python3 run_localGPT_API.py"
+  echo "Restarting the process"
+  cd /home/ec2-user/TEMP/GIT/localGPT
+  nohup python3 run_localGPT_API.py &
+else
+  echo "Nothing was done."
+  exit 1
+fi
