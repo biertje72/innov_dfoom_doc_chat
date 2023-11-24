46fcb7192d316211788a943bbdb50f530edda213
Niels <niels.spaapen@aegon.nl>
2023-11-24 16:13:04 +0100

    test

--- maurice_prompt/prompt.py
+++ maurice_prompt/prompt.py
@@ -70,7 +70,7 @@ def main():
             if last_result["Sources"]:
                 print(f"\nAnswer:\n{last_result['Answer']}")
                 filename = os.path.splitext(os.path.basename(last_result["Sources"][0][0]))[0]
-                print(f"\URL:\n{filename}")
+                print(f"\nURL:\n{filename}")
             else:
                 print(
                     "\nAnswer:\nBased on our documentation, I could not find a relevant answer"
