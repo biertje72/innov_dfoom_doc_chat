54ce82c264a2837cecd6bfe34a6acff119662b54
lycubi1 <mbierhuizen@aegon.nl>
2023-12-01 11:11:45 +0100

    Fix prompt code

--- maurice_prompt/prompt.py
+++ maurice_prompt/prompt.py
@@ -53,7 +53,7 @@ def main():
             elif user_prompt == "r":
                 print("Reloading/ingesting")
                 # response = requests.get(http://10.0.135.48:5110/api/run_ingest)
-                response = requests.get(fhttp://{get_ip_address()}:5110/api/run_ingest)
+                response = requests.get(f"http://{get_ip_address()}:5110/api/run_ingest")
                 print(".. done Reloading/ingesting")
             else:
                 response = query_llm(user_prompt)
